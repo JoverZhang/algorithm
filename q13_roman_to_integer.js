f8c59b5a67d46bd8bf250580c4c812968a8548df
junezqj <junezqj@gmail.com>
2022-10-24 16:11:33 +0800

    one more exercise

---
+++ q13_roman_to_integer.js
@@ -0,0 +1,58 @@
+// var romanToInt = function (s) {
+//     let sum = 0
+//     for (let i = s.length - 1; i >= 0; i--) {
+//         switch (s[i]) {
+//             case 'M':
+//                 sum += 1000
+//                 break;
+//             case 'D':
+//                 sum += 500
+//                 break;
+//             case 'C':
+//                 sum += sum >= 500 ? -100 : 100    //CD,CM
+//                 break;
+//             case 'L':
+//                 sum += 50
+//                 break;
+//             case 'X':
+//                 sum += sum >= 50 ? -10 : 10     //XL,XC
+//                 break;
+//             case 'V':
+//                 sum += 5
+//                 break;
+//             case 'I':
+//                 sum += sum >= 5 ? -1 : 1     //IV,IX
+//                 break;
+//         }
+//     }
+//     return sum
+// };
+
+let numers = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+var romanToInt = function (s) {
+    let res = 0
+    for (let i = 0; i < s.length; i++) {
+        let currNumber = numers[s[i]]
+        let nextNumber = numers[s[i + 1]]
+        if (currNumber < nextNumber) {
+            res = res + nextNumber - currNumber
+            i++
+        } else {
+            res = res + currNumber
+        }
+    }
+    return res
+}
+
+console.log(romanToInt("III"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
