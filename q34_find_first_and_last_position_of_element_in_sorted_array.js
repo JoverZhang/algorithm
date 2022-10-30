de59ce03df6d1f8ae61025bcea190ac0c1c3b4fc
junezqj <junezqj@gmail.com>
2022-10-30 23:43:12 +0800

    add file

---
+++ q34_find_first_and_last_position_of_element_in_sorted_array.js
@@ -0,0 +1,105 @@
+// var searchRange = function (nums, target) {
+//     let res = []
+//     if (nums.length === 0 || nums.indexOf(target) === -1) {
+//         res.push(-1)
+//     } else {
+//         for (let i = 0; i < nums.length; i++) {
+//             if (nums[i] === target) {
+//                 res.push(i)
+//             }
+//         }
+//     }
+//     if (res.length === 1) res.push(res[0])
+//     if (res.length > 2) res.splice(1, res.length - 2)
+//     return res
+// };
+
+// let searchRange = function (nums, target) {
+//     let left = 0
+//     let right = nums.length - 1
+//     const getLeftBorder = function (nums, target) {
+//         let left = 0
+//         let right = nums.length - 1
+//         let leftBorder = -1
+//         while (left <= right) {
+//             let middle = Math.floor((left + right) / 2)
+//             if (nums[middle] === target && nums[middle - 1] < target || nums[middle] === target && nums[middle - 1] === undefined) {
+//                 return leftBorder = middle
+//             } else if (nums[middle] < target) {
+//                 left = middle + 1
+//             } else {
+//                 right = middle - 1
+//             }
+//         }
+//         return leftBorder
+//     }
+//     const getRightBorder = function (nums, target) {
+//         let left = 0
+//         let right = nums.length - 1
+//         let rightBorder = -1
+//         while (left <= right) {
+//             let middle = Math.floor((left + right) / 2)
+//             if (nums[middle] === target && nums[middle + 1] > target || nums[middle] === target && nums[middle + 1] === undefined) {
+//                 return rightBorder = middle
+//             } else if (nums[middle] > target) {
+//                 right = middle - 1
+//             } else {
+//                 left = middle + 1
+//             }
+//         }
+//         return rightBorder
+//     }
+//     return [getLeftBorder(), getRightBorder()]
+// }
+
+// console.log(searchRange([5, 7, 7, 8, 8, 10], 8))
+// console.log(searchRange([5, 7, 7, 8, 8, 10], 6))
+// console.log(searchRange([], 0))
+// console.log(searchRange([1], 1))
+// console.log(searchRange([3, 3, 3], 3))
+
+// const getLeftBorder = function (nums, target) {
+//     let left = 0
+//     let right = nums.length - 1
+//     let leftBorder = -1
+//     while (left <= right) {
+//         let middle = Math.floor((left + right) / 2)
+//         if (nums[middle] === target && nums[middle - 1] < target || nums[middle] === target && nums[middle - 1] === undefined) {
+//             return leftBorder = middle
+//         } else if (nums[middle] < target) {
+//             left = middle + 1
+//         } else {
+//             right = middle - 1
+//         }
+//     }
+//     return leftBorder
+// }
+//
+// console.log(getLeftBorder([5, 7, 7, 8, 8, 10], 8))
+// console.log(getLeftBorder([5, 7, 7, 8, 8, 10], 6))
+// console.log(getLeftBorder([], 0))
+// console.log(getLeftBorder([1], 1))
+// console.log(getLeftBorder([3, 3, 3], 3))
+
+const getRightBorder = function (nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    let rightBorder = -1
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2)
+        if (nums[middle] === target && nums[middle + 1] > target || nums[middle] === target && nums[middle + 1] === undefined) {
+            return rightBorder = middle
+        } else if (nums[middle] > target) {
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
+    }
+    return rightBorder
+}
+
+console.log(getRightBorder([5, 7, 7, 8, 8, 10], 8))
+console.log(getRightBorder([5, 7, 7, 8, 8, 10], 6))
+console.log(getRightBorder([], 0))
+console.log(getRightBorder([1], 1))
+console.log(getRightBorder([3, 3, 3], 3))
