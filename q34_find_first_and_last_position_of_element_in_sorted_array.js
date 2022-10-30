6aff8b054df61f4e82945da38f186db5d7dae5ad
junezqj <junezqj@gmail.com>
2022-10-31 00:01:29 +0800

    update file

--- q34_find_first_and_last_position_of_element_in_sorted_array.js
+++ q34_find_first_and_last_position_of_element_in_sorted_array.js
@@ -14,92 +14,44 @@
 //     return res
 // };
 
-// let searchRange = function (nums, target) {
-//     let left = 0
-//     let right = nums.length - 1
-//     const getLeftBorder = function (nums, target) {
-//         let left = 0
-//         let right = nums.length - 1
-//         let leftBorder = -1
-//         while (left <= right) {
-//             let middle = Math.floor((left + right) / 2)
-//             if (nums[middle] === target && nums[middle - 1] < target || nums[middle] === target && nums[middle - 1] === undefined) {
-//                 return leftBorder = middle
-//             } else if (nums[middle] < target) {
-//                 left = middle + 1
-//             } else {
-//                 right = middle - 1
-//             }
-//         }
-//         return leftBorder
-//     }
-//     const getRightBorder = function (nums, target) {
-//         let left = 0
-//         let right = nums.length - 1
-//         let rightBorder = -1
-//         while (left <= right) {
-//             let middle = Math.floor((left + right) / 2)
-//             if (nums[middle] === target && nums[middle + 1] > target || nums[middle] === target && nums[middle + 1] === undefined) {
-//                 return rightBorder = middle
-//             } else if (nums[middle] > target) {
-//                 right = middle - 1
-//             } else {
-//                 left = middle + 1
-//             }
-//         }
-//         return rightBorder
-//     }
-//     return [getLeftBorder(), getRightBorder()]
-// }
-
-// console.log(searchRange([5, 7, 7, 8, 8, 10], 8))
-// console.log(searchRange([5, 7, 7, 8, 8, 10], 6))
-// console.log(searchRange([], 0))
-// console.log(searchRange([1], 1))
-// console.log(searchRange([3, 3, 3], 3))
-
-// const getLeftBorder = function (nums, target) {
-//     let left = 0
-//     let right = nums.length - 1
-//     let leftBorder = -1
-//     while (left <= right) {
-//         let middle = Math.floor((left + right) / 2)
-//         if (nums[middle] === target && nums[middle - 1] < target || nums[middle] === target && nums[middle - 1] === undefined) {
-//             return leftBorder = middle
-//         } else if (nums[middle] < target) {
-//             left = middle + 1
-//         } else {
-//             right = middle - 1
-//         }
-//     }
-//     return leftBorder
-// }
-//
-// console.log(getLeftBorder([5, 7, 7, 8, 8, 10], 8))
-// console.log(getLeftBorder([5, 7, 7, 8, 8, 10], 6))
-// console.log(getLeftBorder([], 0))
-// console.log(getLeftBorder([1], 1))
-// console.log(getLeftBorder([3, 3, 3], 3))
-
-const getRightBorder = function (nums, target) {
-    let left = 0
-    let right = nums.length - 1
-    let rightBorder = -1
-    while (left <= right) {
-        let middle = Math.floor((left + right) / 2)
-        if (nums[middle] === target && nums[middle + 1] > target || nums[middle] === target && nums[middle + 1] === undefined) {
-            return rightBorder = middle
-        } else if (nums[middle] > target) {
-            right = middle - 1
-        } else {
-            left = middle + 1
+let searchRange = function (nums, target) {
+    const getLeftBorder = function (nums, target) {
+        let left = 0
+        let right = nums.length - 1
+        let leftBorder = -1
+        while (left <= right) {
+            let middle = Math.floor((left + right) / 2)
+            if (nums[middle] === target && nums[middle - 1] < target || nums[middle] === target && nums[middle - 1] === undefined) {
+                return leftBorder = middle
+            } else if (nums[middle] < target) {
+                left = middle + 1
+            } else {
+                right = middle - 1
+            }
         }
+        return leftBorder
     }
-    return rightBorder
+    const getRightBorder = function (nums, target) {
+        let left = 0
+        let right = nums.length - 1
+        let rightBorder = -1
+        while (left <= right) {
+            let middle = Math.floor((left + right) / 2)
+            if (nums[middle] === target && nums[middle + 1] > target || nums[middle] === target && nums[middle + 1] === undefined) {
+                return rightBorder = middle
+            } else if (nums[middle] > target) {
+                right = middle - 1
+            } else {
+                left = middle + 1
+            }
+        }
+        return rightBorder
+    }
+    return [getLeftBorder(nums, target), getRightBorder(nums, target)]
 }
 
-console.log(getRightBorder([5, 7, 7, 8, 8, 10], 8))
-console.log(getRightBorder([5, 7, 7, 8, 8, 10], 6))
-console.log(getRightBorder([], 0))
-console.log(getRightBorder([1], 1))
-console.log(getRightBorder([3, 3, 3], 3))
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8))
+console.log(searchRange([5, 7, 7, 8, 8, 10], 6))
+console.log(searchRange([], 0))
+console.log(searchRange([1], 1))
+console.log(searchRange([3, 3, 3], 3))
